0a3692bc0593e8eab066f3be2e7b42bb6caf783e
Roberto Carlos da Silva Junior <37126197+roberto16477@users.noreply.github.com>
2024-04-02 13:00:58 +0000

    up

--- buble_sort.py
+++ buble_sort.py
@@ -1,11 +1,11 @@
-def bubbleSort(alist):
-    for passnum in range(len(alist)-1,0,-1):
-        for i in range(passnum):
-            if alist[i]>alist[i+1]:
-                temp = alist[i]
-                alist[i] = alist[i+1]
-                alist[i+1] = temp
+def bubbleSort(lista):
+    for passnum in range(len(lista)):  #essa linha me da um for que vai percorrer do inicio ao fim da lista
+        for i in range(len(lista)-1-passnum):  #essa linha evita que eu compare os numeros já organizados (os maiores, que vão estar no final)
+            if lista[i]>lista[i+1]:      #compara o item i com o proximo item(se i for menor que i+1 ele troca) para ver se precisa ou não trocá-los
+                temp = lista[i]
+                lista[i] = lista[i+1]
+                lista[i+1] = temp
 
-alist = [54,26,93,17,77,31,44,55,20]
-bubbleSort(alist)
-print(alist)
+l = [54,26,93,17,77,31,44,55,20]
+bubbleSort(l)
+print(l)
