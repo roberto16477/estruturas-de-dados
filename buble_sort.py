63c0ee016229377d0372b64c7951d68fc9440e02
Roberto Carlos da Silva Junior <37126197+roberto16477@users.noreply.github.com>
2024-04-02 14:01:16 +0000

    up final bubblesort

--- buble_sort.py
+++ buble_sort.py
@@ -1,6 +1,6 @@
 def bubbleSort(lista):
     for passnum in range(len(lista)):  #essa linha me da um for que vai percorrer do inicio ao fim da lista
-        for i in range(len(lista)-1-passnum):  #essa linha evita que eu compare os numeros já organizados (os maiores, que vão estar no final)
+        for i in range((len(lista)-1)-passnum):  #essa linha evita que eu compare os numeros já organizados (os maiores, que vão estar no final)
             if lista[i]>lista[i+1]:      #compara o item i com o proximo item para ver se precisa ou não trocá-los
                 temp = lista[i]     #variavel de apoio
                 lista[i] = lista[i+1]   #faz a primeira troca (passa o item i+1 para a posição i)
