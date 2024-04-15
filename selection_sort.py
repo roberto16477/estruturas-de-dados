a9548389ff9a630785c469774fe023f7a6985f79
Roberto Carlos da Silva Junior <37126197+roberto16477@users.noreply.github.com>
2024-04-15 18:50:18 +0000

    up final selection sort

--- selection_sort.py
+++ selection_sort.py
@@ -1,5 +1,5 @@
 def selection_sort(lista):
-    n=9
+    n= len(lista)
     for i in range(n):
         # Começando no segundo número para usar como comparação
         for j in range (i+1,n):
@@ -8,4 +8,5 @@ def selection_sort(lista):
                 lista[i], lista[j] = lista[j], lista[i]
 
 a = [54,26,93,17,77,31,44,55,20]
-print(selection_sort(a))
+selection_sort(a)
+print(a)
