6b37251672753d04b8276a5a6487a5dc3a2a9f14
Roberto Carlos da Silva Junior <37126197+roberto16477@users.noreply.github.com>
2024-04-16 13:39:31 +0000

    atualização selecton

--- selection_sort.py
+++ selection_sort.py
@@ -1,11 +1,12 @@
 def selection_sort(arr):
     n= len(arr)
     for i in range(n):
+        menor = i
         # Começando no segundo número para usar como comparação
         for j in range (i+1,n):
-            if arr[i] > arr[j]:
-                # Troca de lugar se o número analisado for menor que o número mais à esquerda
-                arr[i], arr[j] = arr[j], arr[i]
+            if arr[menor] > arr[j]:
+                menor = j
+        arr[i], arr[menor] = arr[menor], arr[i]  # Troca de lugar se o número analisado for menor que o número mais à esquerda
 
 a = [54,26,93,17,77,31,44,55,20]
 selection_sort(a)
